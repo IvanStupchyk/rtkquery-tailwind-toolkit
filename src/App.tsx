8464826f87587f4012d6355a8e889ec1af0e9243
IvanStupchuk <hardtrynew@rambler.ru>
2022-11-24 14:31:45 +0100

    feat: initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import {HomePage} from "./pages/home/HomePage";
+import {FavouritesPage} from "./pages/favourites/FavouritesPage";
+import {Navigation} from "./components/Navigation";
 
 function App() {
   return (
-    <h1>
-      HI
-    </h1>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/favourites" element={<FavouritesPage />}/>
+      </Routes>
+    </>
   );
 }
 
